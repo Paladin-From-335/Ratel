b106474ad657d5d44d3b476fe8e2868d310e11c2
andry <andryfilets@gmail.com>
2021-07-19 11:43:45 +0300

    Refactoring controllers

--- core/src/main/java/com/github/ratel/controllers/CommentController.java
+++ core/src/main/java/com/github/ratel/controllers/CommentController.java
@@ -4,6 +4,7 @@ import com.github.ratel.dto.CommentDto;
 import com.github.ratel.entity.Comment;
 import com.github.ratel.services.impl.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,30 +19,35 @@ public class CommentController {
     private CommentService commentService;
 
     @GetMapping
+    @Secured("ROLE_ADMIN")
     public List<Comment> findAllComments() {
         return this.commentService.findAllComments();
     }
 
+    @Secured("ROLE_USER")
     @GetMapping("/{commentsId}")
     public Optional<Comment> findCommentById(@PathVariable long commentsId) {
         return this.commentService.findCommentById(commentsId);
     }
-
+    @Secured("ROLE_USER")
     @GetMapping("/product/{productId}")
     public List<Comment> findAllCommentsByProductId(@PathVariable long productId) {
         return this.commentService.findCommentsByProductId(productId);
     }
 
+    @Secured("ROLE_USER")
     @GetMapping("/user/{userId}")
     public List<Comment> findAllCommentsByUserId(@PathVariable long userId) {
         return this.commentService.findCommentsByUserId(userId);
     }
 
+    @Secured("ROLE_ADMIN")
     @PostMapping("/product/{productId}")
     public Comment createCommentByProductId(@RequestBody CommentDto commentDto, @PathVariable long productId) {
         return this.commentService.saveCommentByProductId(commentDto, productId);
     }
 
+    @Secured("ROLE_ADMIN")
     @PutMapping("/product/{productId}")
     public void editCommentByProductId(@RequestBody Comment comment, @PathVariable long productId) {
         this.commentService.updateCommentByProductId(comment, productId);
